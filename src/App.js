05cd51f9a45ff70f1daedcfb6fb40a72389c61ab
ALTRON\KallisJ <jarrod.kallis@altron.com>
2018-01-02 14:23:24 +0200

    Remove test code for removing axios interceptors

--- src/App.js
+++ src/App.js
@@ -3,23 +3,10 @@ import React from 'react';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
-class App extends React.Component {
-  state = {
-    show: true
-  }
-
-  // componentDidMount() {
-  //   // Test unmounting of a component wrapped by HOC: withErrorHandler
-  //   setTimeout(() => this.setState({ show: false }), 5000);
-  // }
-
-  render() {
-    return (
-      <Layout>
-        {this.state.show ? <BurgerBuilder /> : null}
-      </Layout>
-    );
-  }
-}
+const App = () => (
+  <Layout>
+    <BurgerBuilder />
+  </Layout>
+);
 
 export default App;
