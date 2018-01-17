b94a94a533660c20c9e98df0238cd1398c9d186a
Jarrod Kallis <jarrod.kallis@altron.com>
2018-01-17 11:23:17 +0200

    Hot reloading

--- src/index.js
+++ src/index.js
@@ -7,18 +7,62 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 
 import './index.css';
 
-import App from './App';
+// import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import rootReducer from './rootReducer';
 
 const store = createStore(rootReducer, composeWithDevTools());
 
-ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>,
-  document.getElementById('root')
-);
+// Hot reloading of Redux state
+if (process.env.NODE_ENV !== 'production' && module.hot) {
+  module.hot.accept('./rootReducer', () => {
+    // eslint-disable-next-line
+    const newRootReducer = require('./rootReducer').default;
+    store.replaceReducer(newRootReducer);
+  });
+}
+
+const rootElement = document.getElementById('root');
+
+let render = () => {
+  // eslint-disable-next-line
+  const App = require('./App').default;
+
+  ReactDOM.render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>,
+    rootElement
+  );
+};
+
+if (module.hot) {
+  const renderApp = render;
+
+  render = () => {
+    renderApp();
+  };
+
+  module.hot.accept('./App', () => {
+    setTimeout(render);
+  });
+}
+
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   </Provider>,
+//   document.getElementById('root')
+// );
 registerServiceWorker();
+
+// Hot reloading of the app
+// if (module.hot) {
+//   module.hot.accept();
+// }
+
+render();
