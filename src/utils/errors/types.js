fd9b00de2098c40e2e43e8c11b21510df07c2223
Jarrod Kallis <jarrod.kallis@altron.com>
2018-01-29 12:45:25 +0200

    Login error messages

--- src/utils/errors/types.js
+++ src/utils/errors/types.js
@@ -1,4 +1,5 @@
 export default {
   EMAIL_EXISTS: 'The email address has already been used',
+  EMAIL_NOT_FOUND: 'Invalid username or password',
   INVALID_PASSWORD: 'Invalid username or password'
 };
