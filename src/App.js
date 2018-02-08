c20589087ffc11ed7071c54edfbbcbd8917dd8c8
Jarrod Kallis <jarrod.kallis@gmail.com>
2018-02-08 08:30:38 +0200

    Don't render certain routes if not authenticated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Switch, Route, withRouter } from 'react-router-dom';
 
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
@@ -8,12 +10,12 @@ import Orders from './containers/Orders/Orders';
 import Authorisation from './containers/Authorisation/Authorisation';
 import Logout from './containers/Authorisation/Logout/Logout';
 
-const App = () => (
+const App = ({ isAuthenticated }) => (
   <Layout>
     <Switch>
       <Route path="/" exact component={BurgerBuilder} />
       <Route path="/checkout" component={Checkout} />
-      <Route path="/orders" component={Orders} />
+      {isAuthenticated && <Route path="/orders" component={Orders} />}
       <Route path="/signup" component={Authorisation} />
       <Route path="/logout" component={Logout} />
       <Route
@@ -23,4 +25,19 @@ const App = () => (
   </Layout>
 );
 
-export default App;
+App.propTypes = {
+  isAuthenticated: PropTypes.bool.isRequired
+};
+
+const mapStateToProps = state => ({
+  isAuthenticated: !!state.user.user.idToken
+});
+
+/**
+ * REASON FOR NEEDING withRouter:
+ * User not logged in and types in 'http://localhost:3000/orders' - the app will load 'Route Not Found'.
+ * Then the user clicks the 'Burger Builder' route, but nothing happens.
+ * It's as if the app doesn't detect the change in route.
+ * withRouter sorts this out.
+ */
+export default withRouter(connect(mapStateToProps)(App));
